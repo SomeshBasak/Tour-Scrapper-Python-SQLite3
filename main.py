f3a0458f0af9b36e7b07e47954179aae63c2f8d2
SomeshBasak <someshbasak2016@gmail.com>
2023-10-02 20:18:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,72 +4,75 @@ import selectorlib
 import smtplib, ssl
 import sqlite3
 
-
 URL = "http://programmer100.pythonanywhere.com/tours/"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1'
 }
 
-connection = sqlite3.connect("data.db")
+
+class Event:
+    def scrape(self, url):
+        """Scrape the page source from the URL"""
+        response = requests.get(url, headers=HEADERS)
+        source = response.text
+        return source
+
+    def extract(self,source):
+        extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+        value = extractor.extract(source)["tours"]
+        return value
 
 
-def scrape(url):
-    """Scrape the page source from the URL"""
-    response = requests.get(url, headers=HEADERS)
-    source = response.text
-    return source
+class Email:
+    def send(self, message):
+        host = "smtp.gmail.com"
+        port = 465
+
+        username = "saaarkb16@gmail.com"
+        password = "wrnenkuvnpcicmsj"
+
+        receiver = "someshbasak2016@gmail.com"
+        context = ssl.create_default_context()
+
+        with smtplib.SMTP_SSL(host, port, context=context) as server:
+            server.login(username, password)
+            server.sendmail(username, receiver, message)
+        print("Email was sent")
 
 
-def extract(source):
-    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
-    value = extractor.extract(source)["tours"]
-    return value
+class Database:
+    def __init__(self, database_path):
+        self.connection = sqlite3.connect(database_path)
 
+    def store(self,extracted):
+        row = extracted.split(",")
+        new_row = [item.strip() for item in row]
+        cursor = self.connection.cursor()
+        cursor.execute("INSERT INTO events VALUES(?,?,?)", new_row)
+        self.connection.commit()
 
-def send_email(message):
-    host = "smtp.gmail.com"
-    port = 465
-
-    username = "saaarkb16@gmail.com"    # Put your gmail address inside the quote.
-    password = "wrnenkuvnpcicmsj"     # Put your password inside the quote.
-
-    receiver = "someshbasak2016@gmail.com"     # Put receiver gmail address inside the quote.
-    context = ssl.create_default_context()
-
-    with smtplib.SMTP_SSL(host, port, context=context) as server:
-        server.login(username, password)
-        server.sendmail(username, receiver, message)
-
-    print("Email was sent")
-
-
-def store(extracted):
-    row = extracted.split(",")
-    new_row = [item.strip() for item in row]
-    cursor = connection.cursor()
-    cursor.execute("INSERT INTO events VALUES(?,?,?)", new_row)
-    connection.commit()
-
-
-def read(extracted):
-    row = extracted.split(",")
-    new_row = [item.strip() for item in row]
-    band, city, date = new_row
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?", (band, city, date))
-    rows = cursor.fetchall()
-    return rows
+    def read(self,extracted):
+        row = extracted.split(",")
+        new_row = [item.strip() for item in row]
+        band, city, date = new_row
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?", (band, city, date))
+        rows = cursor.fetchall()
+        return rows
 
 
 if __name__ == "__main__":
     while True:
-        scraped = scrape(URL)
-        extracted = extract(scraped)
+        event = Event()
+        scraped = event.scrape(URL)
+        extracted = event.extract(scraped)
         print(extracted)
 
         if extracted != "No upcoming tours":
-            row = read(extracted)
+            database = Database(database_path="data.db")
+            row = database.read(extracted)
             if not row:
-                store(extracted)
-                send_email(message=f"Hey! New event was started \n {extracted}")
+                database.store(extracted)
+                email = Email()
+                email.send(message=f"Hey! New event was started \n {extracted}")
         time.sleep(1)
